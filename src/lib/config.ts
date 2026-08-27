a80b9d7e2812e1c3053d6df08ddc8e8cf45af1fd
zeynep-kocaman <zeynep.kocamam@enpal.de>
2026-08-27 17:09:38 +0200

    Refactor siteUrl function to use optional chaining

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -58,5 +58,5 @@ export const allowedEmailDomains = (): string[] =>
     .filter(Boolean);
 
 export const siteUrl = (): string =>
-  process.env.NEXT_PUBLIC_SITE_URL ??
+  process.env.SITE_URL ??
   (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
